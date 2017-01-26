96cf10178bce903863ab2232dc2f866824cae90d
Mike Newell <mnewell@lmsed.org>
2017-01-26 19:42:22 +0000

    Add geometic_pattern project

--- that_was_easy/main.js
+++ that_was_easy/main.js
@@ -1,15 +1,8 @@
-function sayThatWasEasy(){
+$("#easy").on("click", sayThatWasEasy);
+   $(document).keypress(delegateKeypress);
+   
+  function sayThatWasEasy(){
 var thatWasEasy = new Audio("that_was_easy.mp3");
 thatWasEasy.play();
-}
-$("#easy").on("click", sayThatWasEasy);
-  $(document).keypress(delegateKeypress);
-  
-  function delegateKeypress (event){
-      if (event.charCode == 32){
-        var thatWasEasy = new Audio("that_was_easy.mp3");
-thatWasEasy.play(); 
-      }
-      
-  }
-    
+  $("#easy").trigger("click");
+    }
